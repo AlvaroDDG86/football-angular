68780cadde78f297b9feb3cdf42b35003cad21c0
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2021-06-19 13:20:08 +0200

    Change api url to https

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,7 +1,7 @@
 
 export class GlobalConstants {
 
-  public static apiURL: string = "http://api.football-data.org/v2/";
+  public static apiURL: string = "https://api.football-data.org/v2/";
 
   public static apiKey: string = "5c42c28db1d34ae1bc57b99d316d29e6";
 
